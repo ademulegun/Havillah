d4ddacbf066e69c6d570a3781cf0a5f0aa8ee805
Gabbum <gabbum360@gmail.com>
2023-03-04 00:38:30 +0100

    i want to rebase so i could correct my migration errors

---
+++ Havillah/Havillah.ApplicationServices/Expense/Dto/GetExpenseDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Havillah.ApplicationServices.Expense.Dto
+{
+    public class GetExpenseDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public decimal Expenditure { get; set; }
+        public DateTime ExpenditureDate { get; set; }
+        public string ContractedBy { get; set; }
+        public string Description { get; }
+    }
+}
---
+++ Havillah/Havillah.ApplicationServices/Expense/Dto/UpdateExpenseDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Havillah.ApplicationServices.Expense.Dto
+{
+    public class UpdateExpenseDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; private set; }
+        public decimal Expenditure { get; private set; }
+        public DateTime ExpenditureDate { get; set; }
+        public string ContractedBy { get; set; }
+        public string Description { get; private set; }
+    }
+}
